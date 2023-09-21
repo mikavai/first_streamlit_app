32c65ca8887eaea93d79811ee48e69c2b99c0238
mikavai <62305255+mikavai@users.noreply.github.com>
2023-09-21 12:41:35 +0300

    Update streamlit_app.py
    a quote was missing

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗  Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.text(🥑🍞 Avocado Toast')
+streamlit.text('🥑🍞 Avocado Toast')
 
 
